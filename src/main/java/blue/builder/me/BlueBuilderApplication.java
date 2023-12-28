671d281ec98645a7de9b332fe323ab68c9d31806
ldu <dldnr3043@naver.com>
2023-12-28 17:31:13 +0900

    spring boot project 구조 푸쉬

---
+++ src/main/java/blue/builder/me/BlueBuilderApplication.java
@@ -0,0 +1,13 @@
+package blue.builder.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blue/builder/me/BlueBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package blue.builder.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
